70e037f1327244ae8bf829dbdb03eba48cf4d572
maximmadan <maximmadan3@gmail.com>
2024-08-10 20:41:27 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+	return (
+		<>
+			<h1 className="text-bold text-5xl font-black text-green-300 transition ease-in delay-50 hover:-translate-y-1 hover:scale-110 hover:text-green-500 duration-300 cursor-pointer ">
+				Vite + React + Tailwind
+			</h1>
+		</>
+	);
+}
+
+export default App;
